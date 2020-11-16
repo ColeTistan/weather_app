881b8e51813692746eb076e1a79f109ab1c9d670
Cole Tistan <coletistan@gmail.com>
2020-11-15 19:44:52 -0500

    fixed dateBuilder function

--- src/app.js
+++ src/app.js
@@ -60,10 +60,10 @@ function dateBuilder(currentDate){
     let months = ["January", "February", "March", "April",
                   "May", "June", "July", "August", "September",
                   "October", "November", "December"];
-    let days = ["Monday", "Tuesday", "Wednesday", "Thursday",
-                "Friday", "Saturday", "Sunday"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+                "Friday", "Saturday"];
     
-    let day = days[currentDate.getDay() - 1];
+    let day = days[currentDate.getDay()];
     let date = currentDate.getDate();
     let month = months[currentDate.getMonth()];
     let year = currentDate.getFullYear();
